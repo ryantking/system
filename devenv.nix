484b0c64d745c11e322677b866c918db2aa1d199
Ryan <ryan@carelesslisper.xyz>
2025-03-18 09:28:50 -0400

    fix: update devenv

--- devenv.nix
+++ devenv.nix
@@ -1,39 +1,22 @@
-{
-  pkgs,
-  lib,
-  config,
-  inputs,
-  ...
-}:
-
-{
+{pkgs, ...}: {
   packages = [
-    pkgs.git
-    pkgs.editorconfig-checker
-    pkgs.ruff
-    pkgs.pylyzer
-    pkgs.isort
-    pkgs.dprint
-    pkgs.nixfmt-rfc-style
-    pkgs.lsp-ai
     pkgs.ansible-language-server
     pkgs.fish-lsp
+    pkgs.lsp-ai
     pkgs.lua-language-server
     pkgs.marksman
     pkgs.markdown-oxide
     pkgs.stylua
+    (pkgs.python3.withPackages (ppkgs: [ppkgs.python-lsp-server]))
   ];
 
   languages = {
     ansible.enable = true;
-
-    nix = {
-      enable = true;
-      lsp.package = pkgs.nixd;
-    };
+    nix.enable = true;
 
     python = {
       enable = true;
+      version = "3.12";
       uv = {
         enable = true;
         sync.enable = true;
@@ -41,18 +24,121 @@
     };
   };
 
+  enterShell = ''
+    source .devenv/state/venv/bin/activate
+    ansible-galaxy install -r ansible/requirements.yml
+  '';
+
+  scripts.format.exec = ''
+    ruff check --fix --unsafe-fixes .
+    dprint fmt
+  '';
+
+  scripts.lint.exec = ''
+    lint-yaml
+    lint-ansible
+    lint-python
+    lint-markdown
+  '';
+
+  scripts.lint-ansible.exec = "ansible-lint .";
+
+  scripts.lint-python.exec = ''
+    ruff check .
+    basedpyright .
+  '';
+
+  scripts.lint-markdown.exec = "markdownlint .";
+
+  scripts.lint-yaml.exec = "yamllint .";
+
   git-hooks.hooks = {
-    nixfmt-rfc-style.enable = true;
-    ruff.enable = true;
-    ruff-format.enable = true;
-    check-builtin-literals.enable = true;
-    check-python.enable = true;
-    fix-encoding-pragma.enable = true;
-    check-yaml.enable = true;
-    yamllint.enable = true;
-    check-merge-conflicts.enable = true;
-    editorconfig-checker.enable = true;
-    trim-trailing-whitespace.enable = true;
+    alejandra.enable = true;
+    check-added-large-files.enable = true;
     end-of-file-fixer.enable = true;
+    trim-trailing-whitespace.enable = true;
+
+    ansible-lint = {
+      enable = true;
+      pass_filenames = false;
+      stages = ["pre-push"];
+      after = ["yamllint"];
+    };
+
+    check-json = {
+      enable = true;
+      fail_fast = true;
+      after = ["check-merge-conflicts"];
+    };
+
+    check-merge-conflicts = {
+      enable = true;
+      fail_fast = true;
+    };
+
+    check-python = {
+      enable = true;
+      fail_fast = true;
+      after = ["check-merge-conflicts"];
+    };
+
+    check-yaml = {
+      enable = true;
+      fail_fast = true;
+      after = ["check-merge-conflicts"];
+    };
+
+    detect-private-keys = {
+      enable = true;
+      fail_fast = true;
+      stages = ["pre-commit" "pre-push"];
+    };
+
+    dprint = {
+      enable = true;
+      name = "dprint formatter";
+      package = pkgs.dprint;
+      entry = "${pkgs.dprint}/bin/dprint fmt";
+      files = "\\.(md|yaml|yml|json|j2|lua)$";
+      pass_filenames = true;
+      after = ["check-json" "check-yaml"];
+    };
+
+    editorconfig-checker = {
+      enable = true;
+      after = ["alejandra" "dprint" "end-of-file-fixer" "trim-trailing-whitespace"];
+    };
+
+    markdownlint = {
+      enable = true;
+      pass_filenames = false;
+      stages = ["pre-push"];
+    };
+
+    pyright = {
+      enable = true;
+      package = pkgs.basedpyright;
+      entry = "${pkgs.basedpyright}/bin/basedpyright";
+      pass_filenames = false;
+      after = ["ruff"];
+      stages = ["pre-push"];
+    };
+
+    ruff = {
+      enable = true;
+      pass_filenames = false;
+      stages = ["pre-push"];
+    };
+
+    typos = {
+      enable = true;
+      stages = ["pre-push"];
+    };
+
+    yamllint = {
+      enable = true;
+      files = ".";
+      stages = ["pre-push"];
+    };
   };
 }
